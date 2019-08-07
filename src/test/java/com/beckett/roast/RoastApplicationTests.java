fa8cad8e8477c8301ac84ece4fbed0497d502a84
shengding <lionding2009@gmail.com>
2019-08-07 21:57:11 +1000

    initial commits

---
+++ src/test/java/com/beckett/roast/RoastApplicationTests.java
@@ -0,0 +1,16 @@
+package com.beckett.roast;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoastApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
